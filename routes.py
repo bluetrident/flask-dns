255df39fbcfcdeed4e6c387b92e882c35ab29ea6
Neil l MacLauchlin <neil@bluetrident.com>
2014-08-01 16:24:16 -0400

    bootstrap

--- routes.py
+++ routes.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, flash
 from forms import ContactForm
 import dns.reversename
 import socket
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 app.secret_key = 'you are a big donkey | 1492'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def lookup():
@@ -21,5 +24,11 @@ def lookup():
     elif request.method == 'GET':
         return render_template('lookup.html', form=form)
     
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
