abe0d735760d43dcf0e8bf11b6908ec57191d4f8
Neil l MacLauchlin <neil@bluetrident.com>
2014-08-01 14:34:39 -0400

    ip address validation

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from wtforms import TextField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, IPAddress
 
 class ContactForm(Form):
-    ipaddr = TextField('IP Address', validators=[DataRequired()])
+    ipaddr = TextField('IP Address', validators=[DataRequired('IP Address is required.'), IPAddress('Invalid IP Address format.')])
     submit = SubmitField('Lookup')
